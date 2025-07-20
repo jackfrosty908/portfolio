aee1e3ab67395ae3505bccdea8d23142e8384bdd
Jack Foster <john.foster908@gmail.com>
2025-07-20 21:20:26 +1200

    update for cloudflare env variables

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,14 +5,24 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 
-const app = new Hono();
+type Bindings = {
+  CORS_ORIGIN?: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.use(logger());
+
 app.use(
   '/*',
   cors({
-    origin: process.env.CORS_ORIGIN || '',
+    origin: (_, c) => {
+      const allowedOrigin = c.env?.CORS_ORIGIN || process.env.CORS_ORIGIN;
+      return allowedOrigin;
+    },
     allowMethods: ['GET', 'POST', 'OPTIONS'],
+    allowHeaders: ['Content-Type'],
+    credentials: true,
   })
 );
 
