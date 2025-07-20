b2c73ae556efb50fa2ec547aa6e0109d72190bb5
Jack Foster <john.foster908@gmail.com>
2025-07-20 20:26:07 +1200

    setup separate files for dev and deployment

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { trpcServer } from '@hono/trpc-server';
 import { createContext } from './lib/context';
 import { appRouter } from './routers/index';
@@ -12,7 +11,7 @@ app.use(logger());
 app.use(
   '/*',
   cors({
-    origin: process.env.CORS_ORIGIN || '*', //TODO: remove global origin
+    origin: process.env.CORS_ORIGIN || '*', //TODO: JF remove once deployments work
     allowMethods: ['GET', 'POST', 'OPTIONS'],
   })
 );
@@ -31,14 +30,4 @@ app.get('/', (c) => {
   return c.text('OK');
 });
 
-import { serve } from '@hono/node-server';
-
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
+export default app;
