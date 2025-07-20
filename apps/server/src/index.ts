1dbcc6a90478df32dbae2d6e70a0e031c9587b91
Jack Foster <john.foster908@gmail.com>
2025-07-20 21:05:49 +1200

    remove * origin

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -42,7 +42,7 @@ app.use(logger());
 app.use(
   '/*',
   cors({
-    origin: process.env.CORS_ORIGIN || '*', //TODO: JF remove once deployments work
+    origin: process.env.CORS_ORIGIN || '',
     allowMethods: ['GET', 'POST', 'OPTIONS'],
   })
 );
